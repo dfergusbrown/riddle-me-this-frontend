34dfbdbf09cc7d080a3bf5e627949adca298e570
dfergusbrown <dfergusbrown@gmail.com>
2024-06-10 18:37:30 -0400

    refactors appearance in MyProfile.jsx

--- src/components/ProfileInstancesList/ProfileInstancesList.jsx
+++ src/components/ProfileInstancesList/ProfileInstancesList.jsx
@@ -1,46 +1,54 @@
 import React from "react";
-import { Button, Flex, Table, Text } from "@radix-ui/themes";
+import { Button, Flex, ScrollArea, Table, Text } from "@radix-ui/themes";
 import { NavLink } from "react-router-dom";
 
 const ProfileInstancesList = ({ participations }) => {
   return (
-    <Table.Root m="4">
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>Location</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>Date</Table.ColumnHeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {!participations.length ? (
-          <Table.Row><Table.Cell colSpan="3">No records to Show</Table.Cell></Table.Row>
-        ) : (
-          participations.map((game, idx) => {
-            const dateObj = new Date(game.hunt_instance.end_time);
-            const endTime = dateObj.toLocaleString("en-US");
+    <Flex m="4" direction="column">
+      <Flex>
+        <Flex width="33%" justify="center">
+          Name
+        </Flex>
+        <Flex width="33%" justify="center">
+          Location
+        </Flex>
+        <Flex width="33%" justify="center">
+          Date
+        </Flex>
+      </Flex>
+      <Flex direction="column" style={{}}>
+        {/* <ScrollArea> */}
+          {!participations.length ? (
+            <Flex width="100%" justify="center">
+              <Text>No records to Show</Text>
+            </Flex>
+          ) : (
+            participations.map((game, idx) => {
+              const dateObj = new Date(game.hunt_instance.end_time);
+              const endTime = dateObj.toLocaleString("en-US");
 
-            return (
-              <Table.Row key={idx}>
-                <Table.Cell>
-                  <Button variant="surface" style={{ padding: "20px" }}>
-                    <NavLink
-                      to={`/hunt-details/${game.hunt_instance.id}/${game.hunt_instance.scavenger_hunt.id}`} style={{textDecoration: 'none', color:'lightgray'}}
-                    >
+              return (
+                <NavLink
+                  to={`/hunt-details/${game.hunt_instance.id}/${game.hunt_instance.scavenger_hunt.id}`}
+                  className="participation-row"
+                  style={{ textDecoration: "none", color: "lightgray" }}
+                >
+                  <Flex width="100%" key={idx}>
+                    <Flex width="33%">
                       {game.hunt_instance.scavenger_hunt.name}
-                    </NavLink>
-                  </Button>
-                </Table.Cell>
-                <Table.Cell>
-                  {game.hunt_instance.scavenger_hunt.location}
-                </Table.Cell>
-                <Table.Cell>{endTime}</Table.Cell>
-              </Table.Row>
-            );
-          })
-        )}
-      </Table.Body>
-    </Table.Root>
+                    </Flex>
+                    <Flex width="33%" justify="center">
+                      {game.hunt_instance.scavenger_hunt.location}
+                    </Flex>
+                    <Flex width="33%">{endTime}</Flex>
+                  </Flex>
+                </NavLink>
+              );
+            })
+          )}
+        {/* </ScrollArea> */}
+      </Flex>
+    </Flex>
   );
 };
 
